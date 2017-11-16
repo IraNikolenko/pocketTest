c093831c1ce02e732e3868ac5fb52c08cae1a886
Ira <nira1705@gmail.com>
2017-11-16 14:57:04 +0200

    Initial Commit

---
+++ PocketsphinxTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PocketsphinxTest
+//
+//  Created by Ira on 16.11.17.
+//  Copyright © 2017 IraNikolenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PocketsphinxTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PocketsphinxTest
+//
+//  Created by Ira on 16.11.17.
+//  Copyright © 2017 IraNikolenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
